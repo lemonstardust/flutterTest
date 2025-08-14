b124722f3ad1e039c5b395f3e2d1027f538267b3
liumeng <46801948+lemonstardust@users.noreply.github.com>
2025-08-15 00:24:54 +0800

    indicator and controller
    add indicator and controller

--- lib/widget/viewpager_demo_page.dart
+++ lib/widget/viewpager_demo_page.dart
@@ -1,23 +1,47 @@
 import 'dart:math' as Math;
 import 'package:another_transformer_page_view/another_transformer_page_view.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ViewPagerDemoPage extends StatelessWidget {
-  final List<Color> colorList = [
+  ViewPagerDemoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const PageView();
+  }
+}
+
+class PageView extends StatefulWidget {
+  const PageView({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PageViewState();
+  }
+}
+
+class _PageViewState extends State<PageView> {
+  List<Color> colorList = [
     Colors.redAccent,
     Colors.blueAccent,
     Colors.greenAccent
   ];
 
-  ViewPagerDemoPage({super.key});
+  // 创建 TransformerPageController 实例
+  TransformerPageController controller = TransformerPageController(
+    itemCount: 3, // 页面数量
+    loop: false, // 是否循环
+    reverse: false, // 是否反转
+  );
+
+  int _curIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme
-          .of(context)
-          .primaryColorDark,
+      backgroundColor: Theme.of(context).primaryColorDark,
       body: Container(
         alignment: Alignment.topLeft,
         child: Column(
@@ -25,51 +49,60 @@ class ViewPagerDemoPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
-          Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 20, top: 30),
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.white,
-                  width: 3.0,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 20, top: 30),
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 3.0,
+                    ),
+                  ),
+                  child: ClipOval(
+                    child: Image.network(
+                        "https://uploads-oss.xstudyedu.com/client/padmanageservice/test/56b3ad5ab369-44e1-8cb9-faa546491a361714390088909.png",
+                        fit: BoxFit.cover,
+                        width: 80,
+                        height: 80, errorBuilder: (context, url, error) {
+                      return Image.asset("assets/pic_tx_default.png",
+                          width: 80, height: 80, fit: BoxFit.cover);
+                    }),
+                  ),
                 ),
-              ),
-              child: ClipOval(
-                child: Image.network(
-                    "https://uploads-oss.xstudyedu.com/client/padmanageservice/test/56b3ad5ab369-44e1-8cb9-faa546491a361714390088909.png",
-                    fit: BoxFit.cover,
-                    width: 80,
-                    height: 80, errorBuilder: (context, url, error) {
-                  return Image.asset("assets/pic_tx_default.png",
-                      width: 80, height: 80, fit: BoxFit.cover);
-                }),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 30, left: 20),
-              child: Text(
-                "name", style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-              // Text(
-              //   "name",
-              //   style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
+                Container(
+                  margin: const EdgeInsets.only(top: 30, left: 20),
+                  child: Text(
+                    "name",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  // Text(
+                  //   "name",
+                  //   style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
               ],
             ),
             // 圆形头像 - 加载网络图片
-
 
             const SizedBox(height: 20),
             Expanded(
               child: TransformerPageView(
                   loop: false,
                   controller: IndexController(),
+                  onPageChanged: (index) {
+                    if (index != null) {
+                      _curIndex = index;
+                    }
+                    if (kDebugMode) {
+                      print("current $index");
+                    }
+                  },
+                  pageController: controller,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
                       decoration: BoxDecoration(
@@ -86,6 +119,31 @@ class ViewPagerDemoPage extends StatelessWidget {
                   },
                   itemCount: 3),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: colorList.asMap().entries.map((entry) {
+                return GestureDetector(
+                    onTap: () {
+                      _curIndex = entry.key;
+                      controller.jumpToPage(_curIndex);
+                      print("onClick ${entry.key}");
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                      width: 20.0,
+                      height: 20.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: entry.key == IndexController().index
+                            ? Colors.white
+                            : Colors.white.withOpacity(0.5),
+                      ),
+                    ));
+              }).toList(),
+            ),
+            SizedBox(
+              height: 50,
+            )
           ],
         ),
       ),
